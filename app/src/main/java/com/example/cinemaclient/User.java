259f87a6a04a6838f13f9e267db2643f7ef41002
maks-buren630501 <maks.bur2809@gmail.com>
2020-02-05 11:14:28 +0300

    fix some logic error and add java doc to all functions

--- app/src/main/java/com/example/cinemaclient/User.java
+++ app/src/main/java/com/example/cinemaclient/User.java
@@ -1,17 +1,21 @@
 package com.example.cinemaclient;
 
+import java.io.IOException;
+import java.net.InetAddress;
 import java.util.ArrayList;
 
 public class User {
+    private static User instance = null;
+    private ClientSocket clientSocket;
     private String nickName;
     private String lastName;
     private String firstName;
-    private ArrayList<Session> listOfSessions;
+    private ArrayList<UserSession> listOfSessions;
 
     /**
      * default constructor without params
      */
-    public User(){
+    private User(){
         this.firstName = "unknow";
         this.lastName = "unknow";
         this.nickName = "unknow";
@@ -23,26 +27,110 @@ public class User {
      * @param lastName of user
      * @param firstName of user
      */
-    public User(String nickName,String lastName, String firstName){
+    private User(String nickName,String lastName, String firstName, InetAddress address, int port){
         this.nickName = nickName;
         this.lastName = lastName;
         this.firstName = firstName;
     }
 
     /**
-     * set method for list of sessions
-     * @param listOfSessions
+     * function for get object of this class
+     * @param nickName of user
+     * @param lastName of user
+     * @param firstName of user
+     * @param address ip address of server
+     * @param port of server
+     * @return object or link on this class
+     * @throws IOException default exception
      */
-    public void setListOfSessions(ArrayList<Session> listOfSessions){
+    public static User getInstance(String nickName,String lastName, String firstName, InetAddress address, int port) throws IOException {
+        if(instance == null){
+            instance = new User(nickName,lastName,firstName,address,port);
+        }
+        return instance;
+    }
+
+    /**
+     * function for get object of this class without params
+     * @return object or link on this class
+     */
+    public static User getInstance(){
+        if(instance == null){
+            instance = new User();
+        }
+        return instance;
+    }
+
+    /**
+     * set method for list of sessions
+     * @param listOfSessions in array list format
+     */
+    public void setListOfSessions(ArrayList<UserSession> listOfSessions){
         this.listOfSessions.clear();
         this.listOfSessions = listOfSessions;
     }
 
     /**
      * method to add new session to list
-     * @param session
+     * @param session of user
      */
-    public void addNewSession(Session session){
+    public void addNewSession(UserSession session){
         this.listOfSessions.add(session);
+    }
+
+    /**
+     * def setter
+     * @param nickName of user
+     */
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    /**
+     * gef getter
+     * @return of user
+     */
+    public String getNickName() {
+        return nickName;
+    }
+
+    /**
+     * def setter
+     * @param lastName of user
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * def getter
+     * @return last name of user
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * def setter
+     * @param firstName of user
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * def getter
+     * @return first name of user
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * def getter
+     * @return client socket
+     */
+    public ClientSocket getClientSocket() {
+        return clientSocket;
     }
 }
